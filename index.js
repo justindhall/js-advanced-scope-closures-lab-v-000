33b913ffa4138dd67bf90e507d219420a712e612
Justin Hall <hall.justin.d@gmail.com>
2018-10-26 00:23:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function produceDrivingRange(blockRange) {
+  return function(origin, terminus) {
+    let startBlock = parseInt(origin);
+    let endBlock = parseInt(terminus);
+    let distanceTraveled = Math.abs(endBlock - startBlock);
+    let difference = blockRange - distanceTraveled;
+    
+    if (difference > 0) {
+      return `within range by ${difference}`;
+    }
+    else {
+      return `${Math.abs(difference)} blocks out of range`;
+    }
+  }
+}
+
+function produceTipCalculator(percentage) {
+  return function(total) {
+    return percentage * total;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
