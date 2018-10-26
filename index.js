c275dd42e5cd6b33ff771d2e18f713a474765f84
Justin Hall <hall.justin.d@gmail.com>
2018-10-26 00:07:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(range) {
   return function(tripDistance) {
-    if (range > tripDistance)
+    if (range > tripDistance) {
       return true;
     else
       return false;
