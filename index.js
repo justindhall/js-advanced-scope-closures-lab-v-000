9f04c14c6eb47d0c931cec18b6410e4aca8ad6f2
Justin Hall <hall.justin.d@gmail.com>
2018-10-26 00:15:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function produceDrivingRange(blockRange) {
   return function(origin, terminus) {
+    let startBlock = parseInt(origin);
+    let endBlock = parseInt(terminus);
+    let distanceTraveled = Math.abs(endBlock - startBlock);
     
-    if (range >= tripDistance) {
-      return true;
+    if (blockRange - distanceTraveled > 0) {
+      return `within range by ;
     }
     else {
       return false;
